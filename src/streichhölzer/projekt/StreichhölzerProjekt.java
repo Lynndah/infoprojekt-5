4ebf5ac85fbd14c0a0c3580b596054fd7bb916e7
lynnbo <lynnbo@macbook-pro>
2018-05-23 15:23:01 +0200

    Mit 2. Spieler

--- src/streichhölzer/projekt/StreichhölzerProjekt.java
+++ src/streichhölzer/projekt/StreichhölzerProjekt.java
@@ -29,14 +29,23 @@ public class StreichhölzerProjekt {
            System.out.println(k+"Ich stand auf einem Nein-Feld");
        } //es ist egal was der Computer jetzt macht, denn er hat eh verloren
        else{
-           int m=i%4;
-           if (m==2) {
-               int k=(int)(Math.random()*3+2);
-           }
+           /*int m=i%4;
+           if (m==0) {
+               int k=4-m;
+           } // wenn der Rest gleich 0 ist, müssen 4 Streichhölzer weggenommen werden
+           else if (m==2) {
+               int k=;
+           } // wenn der Rest gleich 2 ist, muss 1 Streichholz weggenommen werden
+           else if (m==3) {
+               int k=;
+           } //wenn der Rest gleich 3 ist, müssen 3 Streichhölzer weggenommen werden
+           */
+           int y=(int)(Math.random()*3+1); //Anzahl der Streichhölzer die Spieler 2 wegnimmt
+           int k=4-y; // y ist die Anzahl der Hölzer die der Spieler in der vorigen Runde weggenommen hat
            //int k=m-1;
            /*k ist die Anzahl der Hölzer die der Computer wegnehmen muss
            m ist der Rest der übrig bleibt, wenn man durch 4 teilt,
-           m kann nur 2, 3 oder 4 sein
+           m kann nur 0, 2 oder 3 sein
            */
            System.out.println(k+"Ich kann gewinnen");
        }
